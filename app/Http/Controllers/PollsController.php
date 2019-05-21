7b0ade7f71c83e5bd6550d64a76262c5c73517c4
Soaib Momin <smomin20@gmail.com>
2019-05-21 11:49:40 -0400

    Added the BREAD API operations

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Poll;
+
+class PollsController extends Controller
+{
+    public function index(){
+        return response()->json(Poll::get(), 200);
+    }
+
+    public function show($id){
+        $poll = Poll::find($id);
+        if(is_null($poll)){
+            return response()->json(null, 404);
+        }
+        return response()->json(Poll::findOrFail($id), 200);
+    }
+
+    public function store(Request $request){
+        $poll = Poll::create($request->all());
+        return response()->json($poll, 201);
+    }
+
+    public function update(Request $request, Poll $poll){
+        $poll->update($request->all());
+        return response()->json($poll, 200);
+    }
+
+    public function delete(Request $request, Poll $poll){
+        $poll->delete();
+        return response()->json(null, 204);
+    }
+}
